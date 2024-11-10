eeacac55e899729640eb1fb214e46d74426e5820
YasodhaHeshan <yasodhaheshan2002@gmail.com>
2024-11-10 17:48:01 +0530

    Add FontAwesome icons and enhance Todo component display; update TodoWrapper with title and task mapping

--- src/componant/Todo.js
+++ src/componant/Todo.js
@@ -1,6 +1,18 @@
 //sample code snippet of Todo.js
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare} from '@fortawesome/free-solid-svg-icons';
+import { faTrash} from '@fortawesome/free-solid-svg-icons';
 
-export const Todo = () => {
-    return(<div >Todo</div>) ;
+export const Todo = ({task}) => {
+    return(<div className='Todo'>
+        <p>{task.task}</p>
+        <div>
+            
+            <FontAwesomeIcon icon={faPenToSquare}/>
+            <FontAwesomeIcon icon={faTrash} />
+
+        </div>
+
+        </div>) ;
     }
--- src/componant/TodoWrapper.js
+++ src/componant/TodoWrapper.js
@@ -1,6 +1,7 @@
 import React ,{useState} from 'react';
 import { TodoForm } from './TodoForm';
 import { v4 as uuidv4 } from 'uuid';
+import { Todo } from './Todo';
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -14,7 +15,9 @@ export const TodoWrapper = () => {
 
     return (
         <div className='TodoWrapper'>
+            <h1>Get Things Done</h1>
             <TodoForm addTodo={addTodo} />
+            {todos.map((todo, index) => <Todo task={todo} key={index} />)}
         </div>
     );
 }
