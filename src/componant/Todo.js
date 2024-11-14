3d2c3ab9e79d6d8e2611484f84346c5bb99c4f56
YasodhaHeshan <yasodhaheshan2002@gmail.com>
2024-11-14 08:58:02 +0530

    Enhance Todo component with toggle and delete functionality; update TodoWrapper to manage todo state

--- src/componant/Todo.js
+++ src/componant/Todo.js
@@ -1,18 +1,17 @@
-//sample code snippet of Todo.js
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare} from '@fortawesome/free-solid-svg-icons';
-import { faTrash} from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-export const Todo = ({task}) => {
-    return(<div className='Todo'>
-        <p>{task.task}</p>
-        <div>
-            
-            <FontAwesomeIcon icon={faPenToSquare}/>
-            <FontAwesomeIcon icon={faTrash} />
-
+export const Todo = ({ task, toggleComplete, deleteTodo }) => {
+    return (
+        <div className='Todo'>
+            <p onClick={() => toggleComplete(task.id)}
+                className={`${task.completed ? 'completed' : ''}`}>{task.task}</p>
+            <div>
+                <FontAwesomeIcon icon={faPenToSquare} />
+                <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
+            </div>
         </div>
-
-        </div>) ;
-    }
+    );
+}
--- src/componant/TodoWrapper.js
+++ src/componant/TodoWrapper.js
@@ -1,4 +1,4 @@
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 import { TodoForm } from './TodoForm';
 import { v4 as uuidv4 } from 'uuid';
 import { Todo } from './Todo';
@@ -9,15 +9,26 @@ export const TodoWrapper = () => {
     const [todos, setTodos] = useState([]);
 
     const addTodo = (todo) => {
-        setTodos([...todos, {id: uuidv4(),task:todo , completed:false ,isEditing:false}]);
+        setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }]);
         console.log(todos);
     };
+
+    const toggleComplete = id => {
+        setTodos(todos.map(todo => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)));
+    }
+
+    const deleteTodo = id => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    }
 
     return (
         <div className='TodoWrapper'>
             <h1>Get Things Done</h1>
             <TodoForm addTodo={addTodo} />
-            {todos.map((todo, index) => <Todo task={todo} key={index} />)}
+            {todos.map((todo, index) => <Todo task={todo} key={index}
+                toggleComplete={toggleComplete}
+                deleteTodo={deleteTodo}
+            />)}
         </div>
     );
 }
